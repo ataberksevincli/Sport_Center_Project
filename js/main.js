e859f600874a8dc6266603106419ea1e0bb17552
Ataberk Sevinçli <sevincli.ataberk@hotmail.com>
2024-04-28 23:52:10 +0300

    commit updated

--- js/main.js
+++ js/main.js
@@ -281,8 +281,12 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // BMI hesaplama fonksiyonları için event listener ekleme
-  document.getElementById("height-input").addEventListener("input", calculateBMI);
-  document.getElementById("weight-input").addEventListener("input", calculateBMI);
+  document
+    .getElementById("height-input")
+    .addEventListener("input", calculateBMI);
+  document
+    .getElementById("weight-input")
+    .addEventListener("input", calculateBMI);
 });
 
 // Aktif butonu ayarlayan fonksiyon
@@ -297,23 +301,88 @@ function setActive(button) {
 
 // Diğer fonksiyonlar (showYoga, showGroup, vb.)
 function showYoga() {
-  updateClassInfo("Yoga", "images/yoga.jpg", "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id voluptas accusantium molestiae illo hic? Commodi ducimus quo voluptatibus molestias ipsum nam dolorum quia dolor earum.", "Saturday-Sunday: 8:00am - 10:00am", "Monday-Tuesday: 10:00am - 12:00pm", "Wednesday-Friday: 3:00pm - 6:00pm");
+  updateClassInfo(
+    "Yoga",
+    "images/yoga.jpg",
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id voluptas accusantium molestiae illo hic? Commodi ducimus quo voluptatibus molestias ipsum nam dolorum quia dolor earum.",
+    "Saturday-Sunday: 8:00am - 10:00am",
+    "Monday-Tuesday: 10:00am - 12:00pm",
+    "Wednesday-Friday: 3:00pm - 6:00pm"
+  );
 }
 
 function showGroup() {
-  updateClassInfo("Group", "images/group.webp", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam bibendum urna at quam viverra, a fermentum libero vulputate. Aliquam scelerisque urna est, ornare mollis urna.", "Saturday-Sunday: 10:00am - 12:00am", "Monday-Tuesday: 12:00am - 14:00pm", "Wednesday-Friday: 14:00pm - 16:00pm");
+  updateClassInfo(
+    "Group",
+    "images/group.webp",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam bibendum urna at quam viverra, a fermentum libero vulputate. Aliquam scelerisque urna est, ornare mollis urna.",
+    "Saturday-Sunday: 10:00am - 12:00am",
+    "Monday-Tuesday: 12:00am - 14:00pm",
+    "Wednesday-Friday: 14:00pm - 16:00pm"
+  );
 }
 
 function showSolo() {
-  updateClassInfo("Solo", "images/solo.jpg", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras interdum urna vitae dolor ornare ornare. Donec vulputate turpis sed interdum vulputate. Aenean consectetur arcu vel.", "Saturday-Sunday: 12:00am - 14:00am", "Monday-Tuesday: 14:00am - 16:30pm", "Wednesday-Friday: 17:20pm - 19:30pm");
+  updateClassInfo(
+    "Solo",
+    "images/solo.jpg",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras interdum urna vitae dolor ornare ornare. Donec vulputate turpis sed interdum vulputate. Aenean consectetur arcu vel.",
+    "Saturday-Sunday: 12:00am - 14:00am",
+    "Monday-Tuesday: 14:00am - 16:30pm",
+    "Wednesday-Friday: 17:20pm - 19:30pm"
+  );
 }
 
 function showStretching() {
-  updateClassInfo("Stretching", "images/stret.webp", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque massa nunc, volutpat sit amet arcu nec, finibus gravida nulla. Pellentesque habitant morbi tristique senectus et.", "Saturday-Sunday: 18:00am - 20:00am", "Monday-Tuesday: 20:00am - 20:30pm", "Wednesday-Friday: 20:30pm - 22:30pm");
+  updateClassInfo(
+    "Stretching",
+    "images/stret.webp",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque massa nunc, volutpat sit amet arcu nec, finibus gravida nulla. Pellentesque habitant morbi tristique senectus et.",
+    "Saturday-Sunday: 18:00am - 20:00am",
+    "Monday-Tuesday: 20:00am - 20:30pm",
+    "Wednesday-Friday: 20:30pm - 22:30pm"
+  );
 }
 
 function updateClassInfo(title, image, description, time1, time2, time3) {
   document.getElementById("title-1").textContent = `Why are your ${title}?`;
   document.getElementById("content-1").textContent = description;
-  document.getElementById("title-2").textContent = `When
+  document.getElementById(
+    "title-2"
+  ).textContent = `When comes ${title} Your Time.`;
+  document.getElementById("content-2").textContent = time1;
+  document.getElementById("content-3").textContent = time2;
+  document.getElementById("content-4").textContent = time3;
+  document.getElementById("image-1").src = image;
+}
 
+// BMI hesaplama fonksiyonları
+function calculateBMI() {
+  const heightValue = document.getElementById("height-input").value;
+  const weightValue = document.getElementById("weight-input").value;
+
+  if (heightValue && weightValue) {
+    const heightInMeters = heightValue / 100;
+    const bmi = weightValue / (heightInMeters * heightInMeters);
+    updateTrianglePosition(bmi);
+  }
+}
+
+function updateTrianglePosition(bmi) {
+  const triangle = document.getElementById("triangle");
+  let translateXValue = 0;
+
+  if (bmi < 18.5) {
+    translateXValue = -345; // Çok zayıf
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    translateXValue = -115; // Normal
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    translateXValue = 125; // Fazla kilolu
+  } else if (bmi >= 30 && bmi <= 34.9) {
+    translateXValue = 365; // Obezite
+  } else if (bmi >= 35) {
+    translateXValue = 595; // Şiddetli obezite
+  }
+
+  triangle.style.transform = `translateX(${translateXValue}%)`;
+}
